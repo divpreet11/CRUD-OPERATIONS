c20a9a9cb737ba349ec4e827b4894d52953ca42f
divpreet11 <divpreetkaur08@gmail.com>
2022-05-17 18:04:38 +0530

    Data

--- database/con.js
+++ database/con.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/myCRUD", {
-
-}).then(() => {
+mongoose.connect("mongodb://localhost:27017/myCRUD").then(() => {
     console.log(`connection is successful`);
 }).catch((err) => {
     console.log(`no connection`);
